ef6b9c86149b8d827b56516f268575f64a1bfbdf
alstjr5949 <alstjr5949@gmail.com>
2021-10-12 17:44:06 +0900

    화면 넘김 함수 구현

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const mainPage = document.querySelector(".start_page");
 const qnaPage = document.querySelector(".qna");
+const questionBox = document.querySelector(".question");
+const answerBox1 = document.querySelector(".answer1");
+const answerBox2 = document.querySelector(".answer2");
+
+function clickBtn(questionIndex){
+answerBox1.addEventListener("click", function(){
+  next(++questionIndex);
+});
+answerBox2.addEventListener("click", function(){
+  next(++questionIndex);
+})
+}
+
+function next(questionIndex){
+  questionBox.innerHTML = qna[questionIndex].question;
+  answerBox1.innerHTML = qna[questionIndex].a1;
+  answerBox2.innerHTML = qna[questionIndex].a2;
+  clickBtn(questionIndex);
+}
 
 function start(){
   mainPage.style.animation = "fadeOut 1s";
@@ -7,7 +26,9 @@ function start(){
     qnaPage.style.animation = "fadeIn 1s";
     setTimeout(() => {
       mainPage.style.display = "none";
-      qnaPage.style.display = "block";
+      qnaPage.style.display = "flex";
     }, 450)
+    let questionIndex = 0;
+    next(questionIndex);
   }, 450)
 }
