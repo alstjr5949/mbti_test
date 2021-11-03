d0d0fddb88db5aa1f2b187a5df312498d602106e
alstjr5949 <alstjr5949@gmail.com>
2021-11-03 20:58:00 +0900

    kakao-share btn set

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const resultContainer = document.querySelector(".result_container");
 const resetBtn = document.querySelector(".restart_btn");
 const nowQuestion = document.querySelector(".now");
 const linkBtn = document.querySelector(".link_share");
+const kakaoBtn = document.querySelector(".kakao_share");
 let questionIndex = 0;
 let nowQuestionNum = 1;
 const QUESTION_NUM = 12;
@@ -144,3 +145,72 @@ function linkClick(){
 }
 
 linkBtn.addEventListener("click", linkClick);
+
+//카카오톡 공유 이벤트
+function kakaoShare(){
+  Kakao.Link.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: '오늘의 디저트',
+      description: '아메리카노, 빵, 케익',
+      imageUrl:
+        'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+      link: {
+        mobileWebUrl: 'https://developers.kakao.com',
+        androidExecutionParams: 'test',
+      },
+    },
+    itemContent: {
+      profileText: 'Kakao',
+      profileImageUrl: 'http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+      titleImageUrl: 'http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+      titleImageText: 'Cheese cake',
+      titleImageCategory: 'Cake',
+      items: [
+        {
+          item: 'Cake1',
+          itemOp: '1000원',
+        },
+        {
+          item: 'Cake2',
+          itemOp: '2000원',
+        },
+        {
+          item: 'Cake3',
+          itemOp: '3000원',
+        },
+        {
+          item: 'Cake4',
+          itemOp: '4000원',
+        },
+        {
+          item: 'Cake5',
+          itemOp: '5000원',
+        },
+      ],
+      sum: '총 결제금액',
+      sumOp: '15000원',
+    },
+    social: {
+      likeCount: 10,
+      commentCount: 20,
+      sharedCount: 30,
+    },
+    buttons: [
+      {
+        title: '웹으로 이동',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+        },
+      },
+      {
+        title: '앱으로 이동',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+        },
+      },
+    ]
+  });
+}
+
+kakaoBtn.addEventListener("click", kakaoShare);
